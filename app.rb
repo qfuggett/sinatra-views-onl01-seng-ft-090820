5a3838439f09a79ec1e61007dfccee07f4cf05c1
QueenTesa Fuggett <qfuggett@gmail.com>
2020-10-15 17:18:04 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,10 +3,10 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
 	get '/' do
-		erb :info
+		erb :index
 	end
 	
 	get "/info" do
-	  "Testing the info page"
+	  erb :info
 	end
 end
